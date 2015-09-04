fefe28dd86d44d30ee3f4584085a4a58676cc12c
tracer-ics2015 <tracer@njuics.org>
2015-09-04 22:13:52 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  22:13:52 up  9:31,  7 users,  load average: 0.00, 0.01, 0.05 8ae9b2544bf43f16556e6810a52b19697dbdf4fe

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -88,7 +88,7 @@ static int cmd_x(char *args){
 		else{
 			hwaddr_t xy_rm;
 			sscanf(EXPR,"%u",&xy_rm);
-			printf("%x\n",hwaddr_read(xy_rm,4));//buggy?
+			printf("%x\n",lnaddr_read(xy_rm,4));//buggy?
 		}
 	}
 	else
