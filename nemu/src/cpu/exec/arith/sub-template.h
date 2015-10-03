80d6cd57e07017a3c212d4b731250c9c07d5eb13
tracer-ics2015 <tracer@njuics.org>
2015-10-03 10:38:00 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  10:38:00 up  1:34,  5 users,  load average: 0.00, 0.01, 0.05 744b6d7823e2d10df44bb6b95452bb373f620c49

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -4,10 +4,11 @@
 
 #if DATA_BYTE == 2 || DATA_BYTE ==4
 static void do_execute() {
-	DATA_TYPE result = op_dest->val - op_src->simm;
+	DATA_TYPE result = op_dest->val - op_src->val;
 	OPERAND_W(op_dest, result);
 
 	//wait my EFLAGS;
+	//setEFLAGS_CPAZSO((uint32_t)op_dest->val,(uint32_t)op_src->val);
 
 	print_asm_template2();
 }
