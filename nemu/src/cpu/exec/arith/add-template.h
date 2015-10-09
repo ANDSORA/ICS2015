e108ffffbe9d5574254a3ffc85b419d1fda25b62
tracer-ics2015 <tracer@njuics.org>
2015-10-09 12:25:04 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  12:25:04 up  3:57,  3 users,  load average: 0.08, 0.03, 0.05 5f5fd8dc3668de0e93460e8bb072786ff352e30

--- nemu/src/cpu/exec/arith/add-template.h
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -1,21 +1,19 @@
 #include "cpu/exec/template-start.h"
 #include "cpu/exec/eflags.h"
 
-#define instr sub
+#define instr add
 
 #if DATA_BYTE == 2 || DATA_BYTE == 4
 static void do_execute() {
-	DATA_TYPE result = op_dest->val - op_src->val;
+	DATA_TYPE result = op_dest->val + op_src->val;
 	OPERAND_W(op_dest, result);
 
-	/* update my EFLAGS -ANDSORA */
-	setEFLAGS_CPAZSO((uint32_t)op_dest->val,(uint32_t)op_src->val,1);
-	//testEFLAGS();
+	setEFLAGS_CPAZSO((uint32_t)op_dest->val,(uint32_t)op_src->val,0);
 
 	print_asm_template2();
 }
 
-make_instr_helper(si2rm)
+make_instr_helper(r2rm)
 #endif
 
 #include "cpu/exec/template-end.h"
