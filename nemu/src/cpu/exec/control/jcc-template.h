0b82ba876dc56f12afdf972874aad0acabc99e87
tracer-ics2015 <tracer@njuics.org>
2015-10-11 13:13:08 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  13:13:08 up  4:54,  4 users,  load average: 0.04, 0.05, 0.05 dc142e43c406ed75f0e3fce52c1fb9a7a906414b

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -1,18 +1,70 @@
 #include "cpu/exec/template-start.h"
 
 #if DATA_BYTE == 1 || DATA_BYTE == 4
-#if instr == je
+#if instr == jo
 static void concat3(instr,_execute_,SUFFIX)() {
 	if(cpu.ZF==1) cpu.eip += op_src->val;
-	print_asm("je\t0x%x",cpu.eip+DATA_BYTE+1);}
+	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
+#elif instr == jno
+static void concat3(instr,_execute_,SUFFIX)() {
+	if(cpu.ZF==1) cpu.eip += op_src->val;
+	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
+#elif instr == jb
+static void concat3(instr,_execute_,SUFFIX)() {
+	if(cpu.ZF==1) cpu.eip += op_src->val;
+	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
+#elif instr == jae
+static void concat3(instr,_execute_,SUFFIX)() {
+	if(cpu.ZF==1) cpu.eip += op_src->val;
+	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
+#elif instr == je
+static void concat3(instr,_execute_,SUFFIX)() {
+	if(cpu.ZF==1) cpu.eip += op_src->val;
+	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
+#elif instr == jne
+static void concat3(instr,_execute_,SUFFIX)() {
+	if(cpu.ZF==1) cpu.eip += op_src->val;
+	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
 #elif instr == jbe
 static void concat3(instr,_execute_,SUFFIX)() {
 	if(cpu.CF==1||cpu.ZF==1) cpu.eip += op_src->val;
 	print_asm("jbe\t0x%x",cpu.eip+DATA_BYTE+1);}
+#elif instr == ja
+static void concat3(instr,_execute_,SUFFIX)() {
+	if(cpu.ZF==1) cpu.eip += op_src->val;
+	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
+#elif instr == js
+static void concat3(instr,_execute_,SUFFIX)() {
+	if(cpu.ZF==1) cpu.eip += op_src->val;
+	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
+#elif instr == jns
+static void concat3(instr,_execute_,SUFFIX)() {
+	if(cpu.ZF==1) cpu.eip += op_src->val;
+	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
+#elif instr == jp
+static void concat3(instr,_execute_,SUFFIX)() {
+	if(cpu.ZF==1) cpu.eip += op_src->val;
+	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
+#elif instr == jnp
+static void concat3(instr,_execute_,SUFFIX)() {
+	if(cpu.ZF==1) cpu.eip += op_src->val;
+	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
+#elif instr == jl
+static void concat3(instr,_execute_,SUFFIX)() {
+	if(cpu.ZF==1) cpu.eip += op_src->val;
+	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
+#elif instr == jge
+static void concat3(instr,_execute_,SUFFIX)() {
+	if(cpu.ZF==1) cpu.eip += op_src->val;
+	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
 #elif instr == jle
 static void concat3(instr,_execute_,SUFFIX)() {
 	if(cpu.SF!=cpu.OF||cpu.ZF==1) cpu.eip += op_src->val;
 	print_asm("jle\t0x%x",cpu.eip+DATA_BYTE+1);}
+#elif instr == jg
+static void concat3(instr,_execute_,SUFFIX)() {
+	if(cpu.ZF==1) cpu.eip += op_src->val;
+	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
 #endif
 
 make_helper(concat3(instr,_si_,SUFFIX)){
--- nemu/src/cpu/exec/control/jcc.c
+++ nemu/src/cpu/exec/control/jcc.c
@@ -2,13 +2,52 @@
 
 #define DATA_BYTE 1
 
+#define instr jo
+#include "jcc-template.h"
+#undef instr
+#define instr jno
+#include "jcc-template.h"
+#undef instr
+#define instr jb
+#include "jcc-template.h"
+#undef instr
+#define instr jae
+#include "jcc-template.h"
+#undef instr
 #define instr je
+#include "jcc-template.h"
+#undef instr
+#define instr jne
 #include "jcc-template.h"
 #undef instr
 #define instr jbe
 #include "jcc-template.h"
 #undef instr
+#define instr ja
+#include "jcc-template.h"
+#undef instr
+#define instr js
+#include "jcc-template.h"
+#undef instr
+#define instr jns
+#include "jcc-template.h"
+#undef instr
+#define instr jp
+#include "jcc-template.h"
+#undef instr
+#define instr jnp
+#include "jcc-template.h"
+#undef instr
+#define instr jl
+#include "jcc-template.h"
+#undef instr
+#define instr jge
+#include "jcc-template.h"
+#undef instr
 #define instr jle
+#include "jcc-template.h"
+#undef instr
+#define instr jg
 #include "jcc-template.h"
 #undef instr
 
@@ -18,13 +57,52 @@
 
 #define DATA_BYTE 2
 
+#define instr jo
+#include "jcc-template.h"
+#undef instr
+#define instr jno
+#include "jcc-template.h"
+#undef instr
+#define instr jb
+#include "jcc-template.h"
+#undef instr
+#define instr jae
+#include "jcc-template.h"
+#undef instr
 #define instr je
+#include "jcc-template.h"
+#undef instr
+#define instr jne
 #include "jcc-template.h"
 #undef instr
 #define instr jbe
 #include "jcc-template.h"
 #undef instr
+#define instr ja
+#include "jcc-template.h"
+#undef instr
+#define instr js
+#include "jcc-template.h"
+#undef instr
+#define instr jns
+#include "jcc-template.h"
+#undef instr
+#define instr jp
+#include "jcc-template.h"
+#undef instr
+#define instr jnp
+#include "jcc-template.h"
+#undef instr
+#define instr jl
+#include "jcc-template.h"
+#undef instr
+#define instr jge
+#include "jcc-template.h"
+#undef instr
 #define instr jle
+#include "jcc-template.h"
+#undef instr
+#define instr jg
 #include "jcc-template.h"
 #undef instr
 
@@ -34,13 +112,52 @@
 
 #define DATA_BYTE 4
 
+#define instr jo
+#include "jcc-template.h"
+#undef instr
+#define instr jno
+#include "jcc-template.h"
+#undef instr
+#define instr jb
+#include "jcc-template.h"
+#undef instr
+#define instr jae
+#include "jcc-template.h"
+#undef instr
 #define instr je
+#include "jcc-template.h"
+#undef instr
+#define instr jne
 #include "jcc-template.h"
 #undef instr
 #define instr jbe
 #include "jcc-template.h"
 #undef instr
+#define instr ja
+#include "jcc-template.h"
+#undef instr
+#define instr js
+#include "jcc-template.h"
+#undef instr
+#define instr jns
+#include "jcc-template.h"
+#undef instr
+#define instr jp
+#include "jcc-template.h"
+#undef instr
+#define instr jnp
+#include "jcc-template.h"
+#undef instr
+#define instr jl
+#include "jcc-template.h"
+#undef instr
+#define instr jge
+#include "jcc-template.h"
+#undef instr
 #define instr jle
+#include "jcc-template.h"
+#undef instr
+#define instr jg
 #include "jcc-template.h"
 #undef instr
 
