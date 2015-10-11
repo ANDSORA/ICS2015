462b856f96defe01fb35ed4dd73f0136c82704f6
tracer-ics2015 <tracer@njuics.org>
2015-10-11 15:57:15 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  15:57:15 up  7:38,  4 users,  load average: 0.00, 0.01, 0.05 11b9674621fbbf709d2a33cc54c4e5d65e226068

--- nemu/src/cpu/exec/control/jcc.c
+++ nemu/src/cpu/exec/control/jcc.c
@@ -3,115 +3,83 @@
 #define DATA_BYTE 1
 
 #define instr jo
-#define instr_num 1
 #define instr_value (cpu.OF==1)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jno
-#define instr_num 2
 #define instr_value (cpu.OF==0)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jb
-#define instr_num 3
 #define instr_value (cpu.CF==1)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jae
-#define instr_num 4
 #define instr_value (cpu.CF==0)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr je
-#define instr_num 5
 #define instr_value (cpu.ZF==1)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jne
-#define instr_num 6
 #define instr_value (cpu.ZF==0)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jbe
-#define instr_num 7
 #define instr_value (cpu.CF==1||cpu.ZF==1)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr ja
-#define instr_num 8
 #define instr_value (cpu.CF==0&&cpu.ZF==0)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr js
-#define instr_num 9
 #define instr_value (cpu.SF==1)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jns
-#define instr_num 10
 #define instr_value (cpu.SF==0)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jp
-#define instr_num 11
 #define instr_value (cpu.PF==1)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jnp
-#define instr_num 12
 #define instr_value (cpu.PF==0)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jl
-#define instr_num 13
 #define instr_value (cpu.SF!=cpu.OF)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jge
-#define instr_num 14
 #define instr_value (cpu.SF==cpu.OF)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jle
-#define instr_num 15
 #define instr_value ((cpu.SF!=cpu.OF)||cpu.ZF==1)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jg
-#define instr_num 16
 #define instr_value ((cpu.SF==cpu.OF)&&cpu.ZF==0)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 
@@ -122,115 +90,83 @@
 #define DATA_BYTE 4
 
 #define instr jo
-#define instr_num 1
 #define instr_value (cpu.OF==1)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jno
-#define instr_num 2
 #define instr_value (cpu.OF==0)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jb
-#define instr_num 3
 #define instr_value (cpu.CF==1)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jae
-#define instr_num 4
 #define instr_value (cpu.CF==0)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr je
-#define instr_num 5
 #define instr_value (cpu.ZF==1)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jne
-#define instr_num 6
 #define instr_value (cpu.ZF==0)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jbe
-#define instr_num 7
 #define instr_value (cpu.CF==1||cpu.ZF==1)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr ja
-#define instr_num 8
 #define instr_value (cpu.CF==0&&cpu.ZF==0)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr js
-#define instr_num 9
 #define instr_value (cpu.SF==1)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jns
-#define instr_num 10
 #define instr_value (cpu.SF==0)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jp
-#define instr_num 11
 #define instr_value (cpu.PF==1)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jnp
-#define instr_num 12
 #define instr_value (cpu.PF==0)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jl
-#define instr_num 13
 #define instr_value (cpu.SF!=cpu.OF)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jge
-#define instr_num 14
 #define instr_value (cpu.SF==cpu.OF)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jle
-#define instr_num 15
 #define instr_value ((cpu.SF!=cpu.OF)||cpu.ZF==1)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 #define instr jg
-#define instr_num 16
 #define instr_value ((cpu.SF==cpu.OF)&&cpu.ZF==0)
 #include "jcc-template.h"
-#undef instr_num
 #undef instr_value
 #undef instr
 
