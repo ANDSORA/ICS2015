ed8fcfea8707608159bfb3041ac8ded3b924c1cb
tracer-ics2015 <tracer@njuics.org>
2015-10-11 17:22:55 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  17:22:55 up  9:03,  4 users,  load average: 0.06, 0.03, 0.05 c7885dddcbec6c17db53b422568c1a47b113647d

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -3,15 +3,19 @@
 
 #define instr cmp
 
-#if DATA_BYTE == 2 || DATA_BYTE == 4
 static void do_execute() {
 	setEFLAGS_ALU((uint32_t)op_dest->val,(uint32_t)op_src->val,1);
 
 	print_asm_template2();
 }
 
-make_instr_helper(si2rm)
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
 make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
 #endif
 
 #include "cpu/exec/template-end.h"
