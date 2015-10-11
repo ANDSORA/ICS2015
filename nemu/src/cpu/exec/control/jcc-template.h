ccf2e8f9cfba4fb214c8cc51671e06435b6f43e4
tracer-ics2015 <tracer@njuics.org>
2015-10-11 15:57:58 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  15:57:58 up  7:39,  4 users,  load average: 0.08, 0.03, 0.05 21fe3830600b13495a9d611c37535bddb77a363

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -2,12 +2,10 @@
 
 #if DATA_BYTE == 1 || DATA_BYTE == 4
 static void concat3(instr,_execute_,SUFFIX)() {
-	printf("get in jo\n");
 	if(instr_value) cpu.eip += op_src->val;
 	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
 
 make_helper(concat3(instr,_si_,SUFFIX)){
-	printf("get in jcc\n");
 	return idex(eip,concat(decode_si_,SUFFIX),concat3(instr,_execute_,SUFFIX));
 }
 #endif
