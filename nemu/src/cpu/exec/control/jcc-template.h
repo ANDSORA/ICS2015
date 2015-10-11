5b3586bf52f10cf74a4433fac35c3554966e7eef
tracer-ics2015 <tracer@njuics.org>
2015-10-11 13:22:06 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  13:22:06 up  5:03,  4 users,  load average: 0.00, 0.01, 0.05 af988c2c7841959d5b6000102e861400f00b18bb

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -3,19 +3,19 @@
 #if DATA_BYTE == 1 || DATA_BYTE == 4
 #if instr == jo
 static void concat3(instr,_execute_,SUFFIX)() {
-	if(cpu.ZF==1) cpu.eip += op_src->val;
+	if(cpu.OF==1) cpu.eip += op_src->val;
 	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
 #elif instr == jno
 static void concat3(instr,_execute_,SUFFIX)() {
-	if(cpu.ZF==1) cpu.eip += op_src->val;
+	if(cpu.OF==0) cpu.eip += op_src->val;
 	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
 #elif instr == jb
 static void concat3(instr,_execute_,SUFFIX)() {
-	if(cpu.ZF==1) cpu.eip += op_src->val;
+	if(cpu.CF==1) cpu.eip += op_src->val;
 	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
 #elif instr == jae
 static void concat3(instr,_execute_,SUFFIX)() {
-	if(cpu.ZF==1) cpu.eip += op_src->val;
+	if(cpu.CF==0) cpu.eip += op_src->val;
 	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
 #elif instr == je
 static void concat3(instr,_execute_,SUFFIX)() {
@@ -23,7 +23,7 @@ static void concat3(instr,_execute_,SUFFIX)() {
 	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
 #elif instr == jne
 static void concat3(instr,_execute_,SUFFIX)() {
-	if(cpu.ZF==1) cpu.eip += op_src->val;
+	if(cpu.ZF==0) cpu.eip += op_src->val;
 	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
 #elif instr == jbe
 static void concat3(instr,_execute_,SUFFIX)() {
@@ -31,31 +31,31 @@ static void concat3(instr,_execute_,SUFFIX)() {
 	print_asm("jbe\t0x%x",cpu.eip+DATA_BYTE+1);}
 #elif instr == ja
 static void concat3(instr,_execute_,SUFFIX)() {
-	if(cpu.ZF==1) cpu.eip += op_src->val;
+	if(cpu.CF==0&&cpu.ZF==1) cpu.eip += op_src->val;
 	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
 #elif instr == js
 static void concat3(instr,_execute_,SUFFIX)() {
-	if(cpu.ZF==1) cpu.eip += op_src->val;
+	if(cpu.SF==1) cpu.eip += op_src->val;
 	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
 #elif instr == jns
 static void concat3(instr,_execute_,SUFFIX)() {
-	if(cpu.ZF==1) cpu.eip += op_src->val;
+	if(cpu.SF==0) cpu.eip += op_src->val;
 	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
 #elif instr == jp
 static void concat3(instr,_execute_,SUFFIX)() {
-	if(cpu.ZF==1) cpu.eip += op_src->val;
+	if(cpu.PF==1) cpu.eip += op_src->val;
 	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
 #elif instr == jnp
 static void concat3(instr,_execute_,SUFFIX)() {
-	if(cpu.ZF==1) cpu.eip += op_src->val;
+	if(cpu.PF==0) cpu.eip += op_src->val;
 	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
 #elif instr == jl
 static void concat3(instr,_execute_,SUFFIX)() {
-	if(cpu.ZF==1) cpu.eip += op_src->val;
+	if(cpu.SF!=cpu.OF) cpu.eip += op_src->val;
 	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
 #elif instr == jge
 static void concat3(instr,_execute_,SUFFIX)() {
-	if(cpu.ZF==1) cpu.eip += op_src->val;
+	if(cpu.SF==cpu.OF) cpu.eip += op_src->val;
 	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
 #elif instr == jle
 static void concat3(instr,_execute_,SUFFIX)() {
@@ -63,7 +63,7 @@ static void concat3(instr,_execute_,SUFFIX)() {
 	print_asm("jle\t0x%x",cpu.eip+DATA_BYTE+1);}
 #elif instr == jg
 static void concat3(instr,_execute_,SUFFIX)() {
-	if(cpu.ZF==1) cpu.eip += op_src->val;
+	if(cpu.SF==cpu.OF&&cpu.ZF==0) cpu.eip += op_src->val;
 	print_asm( str(instr) "\t0x%x",cpu.eip+DATA_BYTE+1);}
 #endif
 
