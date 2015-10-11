87c59750b770ce003f8deb34bfb32eadc60f91a6
141220121-XuYin <1035378548@qq.com>
2015-10-11 11:21:10 +0800

    (PA2 part2) Midway in op "jcc"

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -1,24 +1,25 @@
 #include "cpu/exec/template-start.h"
 
-#define instr jcc
-
 #if DATA_BYTE == 1 || DATA_BYTE == 4
-static void concat(je_execute_,SUFFIX)() {
+#if instr == je
+static void concat3(instr,_execute_,SUFFIX)() {
 	if(cpu.ZF==1) cpu.eip += op_src->val;
 	print_asm("je\t0x%x",cpu.eip+DATA_BYTE+1);}
-static void concat(jbe_execute_,SUFFIX)() {
+#endif
+#if instr == jbe
+static void concat3(instr,_execute_,SUFFIX)() {
 	if(cpu.CF==1||cpu.ZF==1) cpu.eip += op_src->val;
 	print_asm("jbe\t0x%x",cpu.eip+DATA_BYTE+1);}
-static void concat(jle_execute_,SUFFIX)() {
+#endif
+#if instr == jle
+static void concat3(instr,_execute_,SUFFIX)() {
 	if(cpu.SF!=cpu.OF||cpu.ZF==1) cpu.eip += op_src->val;
 	print_asm("jle\t0x%x",cpu.eip+DATA_BYTE+1);}
+#endif
 
-make_helper(concat(je_si_,SUFFIX)){
-	return idex(eip,concat(decode_si_,SUFFIX),concat(je_execute_,SUFFIX));}
-make_helper(concat(jbe_si_,SUFFIX)){
-	return idex(eip,concat(decode_si_,SUFFIX),concat(jbe_execute_,SUFFIX));}
-make_helper(concat(jle_si_,SUFFIX)){
-	return idex(eip,concat(decode_si_,SUFFIX),concat(jle_execute_,SUFFIX));}
+make_helper(concat3(instr,_si_,SUFFIX)){
+	return idex(eip,concat(decode_si_,SUFFIX),concat3(instr,_execute_,SUFFIX));
+}
 #endif
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/control/jcc.c
+++ nemu/src/cpu/exec/control/jcc.c
@@ -1,13 +1,47 @@
 #include "cpu/exec/helper.h"
 
 #define DATA_BYTE 1
+
+#define instr je
 #include "jcc-template.h"
+#undef instr
+#define instr jbe
+#include "jcc-template.h"
+#undef instr
+#define instr jle
+#include "jcc-template.h"
+#undef instr
+
 #undef DATA_BYTE
+
+
 
 #define DATA_BYTE 2
+
+#define instr je
 #include "jcc-template.h"
+#undef instr je
+#define instr jbe
+#include "jcc-template.h"
+#undef instr jbe
+#define instr jle
+#include "jcc-template.h"
+#undef instr jle
+
 #undef DATA_BYTE
 
+
+
 #define DATA_BYTE 4
+
+#define instr je
 #include "jcc-template.h"
+#undef instr je
+#define instr jbe
+#include "jcc-template.h"
+#undef instr jbe
+#define instr jle
+#include "jcc-template.h"
+#undef instr jle
+
 #undef DATA_BYTE
