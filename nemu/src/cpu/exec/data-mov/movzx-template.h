cbe7a5280299120113cd10fe7936a48f7a8536d8
tracer-ics2015 <tracer@njuics.org>
2015-10-13 01:19:42 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  01:19:42 up  1:47,  3 users,  load average: 0.01, 0.02, 0.05 4390773362b1fe73a9df8e621bcd7ad7578b328a

--- nemu/src/cpu/exec/data-mov/movzx-template.h
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -4,7 +4,7 @@
 
 #if DATA_BYTE == 4
 make_helper(concat(movzx_rmw2r_, SUFFIX)) {
-	int len = concat(decode_rmb2r_, SUFFIX)(eip + 1);
+	int len = concat(decode_rmw2r_, SUFFIX)(eip + 1);
 	OPERAND_W(op_dest, (DATA_TYPE)op_src->val);
 
 	print_asm("movzw" str(SUFFIX) " %s,%s", op_src->str, op_dest->str);
