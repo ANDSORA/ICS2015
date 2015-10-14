db2c084666e76685d557e1a39683a0117fde9231
tracer-ics2015 <tracer@njuics.org>
2015-10-14 15:38:51 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  15:38:51 up 7 min,  3 users,  load average: 0.02, 0.03, 0.03 29551a0e8bbd9832e1c59368f279c200dd0d1cba

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -2,6 +2,16 @@
 
 #define instr push
 
+#if DATA_BYTE == 1
+make_helper(push_i_b) {
+	int len=decode_i_b(eip);
+	cpu.esp -= 4;
+	swaddr_write(cpu.esp, 4, op_src->val);
+
+	print_asm_template1();
+	return len+1;
+}
+#elif DATA_BYTE == 2 || DATA_BYTE == 4
 static void do_execute() {
 	REG(R_ESP) -= DATA_BYTE;
 	MEM_W( REG(R_ESP),  op_src->val);
@@ -10,8 +20,6 @@ static void do_execute() {
 }
 
 make_instr_helper(i)
-
-#if DATA_BYTE == 2 || DATA_BYTE == 4
 make_instr_helper(rm)
 make_instr_helper(r)
 #endif
