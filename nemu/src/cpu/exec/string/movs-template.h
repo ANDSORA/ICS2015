cdc1954f34a0e1205d9988cf2acffe05353480be
tracer-ics2015 <tracer@njuics.org>
2015-10-14 19:31:08 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  19:31:08 up  3:59,  4 users,  load average: 0.06, 0.03, 0.05 a3142872cbbb153a3aa0e056e29e0192ae1a7cbf

---
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -0,0 +1,19 @@
+#include "cpu/exec/template-start.h"
+
+#define instr movs
+
+make_helper( concat(movs_,SUFFIX) ) {
+	MEM_W(cpu.edi, MEM_R(cpu.esi));
+	if(cpu.DF){
+		cpu.edi -= DATA_BYTE;
+		cpu.esi -= DATA_BYTE;
+	}
+	else{
+		cpu.edi += DATA_BYTE;
+		cpu.esi += DATA_BYTE;
+	}
+	print_asm(str(instr) str(SUFFIX));
+	return 1;
+}
+
+#include "cpu/exec/template-end.h"
