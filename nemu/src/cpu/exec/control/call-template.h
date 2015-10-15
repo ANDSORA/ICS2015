f78b73f09c024b90c42191a1571bbd218863fbe6
tracer-ics2015 <tracer@njuics.org>
2015-10-15 19:26:40 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  19:26:40 up  1:14,  4 users,  load average: 0.00, 0.01, 0.04 45b8e7f06bfa16f51cf065684bce77d3858ca908

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -2,25 +2,32 @@
 
 #define instr call
 
-/* sometimes maybe useful -ANDSORA
-*static void do_execute() {
-*	cpu.esp -= DATA_BYTE;
-*	MEM_W(cpu.esp,cpu.eip+5);
-*	cpu.eip += op_src->val;
-*
-*	print_asm_template1();
-*}
-*/
+make_helper( concat(call_i_, SUFFIX) ) {
+	int len = concat(decode_i_, SUFFIX)(eip+1);
 
-static void do_execute() {
-	REG(R_ESP) -= DATA_BYTE;
-	MEM_W( REG(R_ESP), (DATA_TYPE)(cpu.eip+DATA_BYTE+1));
+	cpu.esp -= DATA_BYTE;
+	MEM_W( cpu.esp, (DATA_TYPE)(eip+len+1) );
+
 	cpu.eip += op_src->val;
 	if(DATA_BYTE==2) cpu.eip = cpu.eip & 0xffff;
 
-	print_asm("call\t0x%x",cpu.eip+DATA_BYTE+1);
-}
+	print_asm("call\t0x%x",cpu.eip+len+1);
 
-make_instr_helper(i)
+	return len+1;
+}
+make_helper( concat(call_rm_, SUFFIX) ) {
+	int len = concat(decode_rm_, SUFFIX)(eip+1);
+
+	cpu.esp -= DATA_BYTE;
+	MEM_W( cpu.esp, (DATA_TYPE)(eip+len+1) );
+
+	cpu.eip = MEM_R(op_src->val)-len-1;
+	if(DATA_BYTE==2) cpu.eip = cpu.eip & 0xffff;
+
+	print_asm("call\t0x%x",cpu.eip+len+1);
+
+	return len+1;
+
+}
 
 #include "cpu/exec/template-end.h"
