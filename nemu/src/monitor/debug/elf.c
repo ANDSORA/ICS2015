0cb029793db618ffbffc0f144f6bb43dade63bdf
tracer-ics2015 <tracer@njuics.org>
2015-10-19 20:24:52 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  20:24:52 up  6:50,  5 users,  load average: 0.00, 0.01, 0.05 738dcc92b2ba8e8b9cbf3c8b5a8d6e871f65f87

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -90,7 +90,7 @@ uint32_t Elf_Value_Read(char* Value_Name, bool* success){
 	int i;
 	printf("%d\n",STB_GLOBAL);
 	for(i=0;i<nr_symtab_entry;++i){
-		printf("%d\t%d\t%d\n",symtab[i].st_name,symtab[i].st_value,symtab[i].st_info);
+		printf("%d\t0x%x\t%d\n",symtab[i].st_name,symtab[i].st_value,symtab[i].st_info);
 		/*
 		if(symtab[i].st_info==STB_GLOBAL&&!strcmp(Value_Name,strtab+symtab[i].st_name)){
 			*success=1;
