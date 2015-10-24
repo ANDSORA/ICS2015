0f44c4bde9e0608489e64f8866b504e51f66d3a5
tracer-ics2015 <tracer@njuics.org>
2015-10-24 17:19:18 +0800

    >  compile kernel 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  17:19:18 up  1:49,  4 users,  load average: 0.01, 0.05, 0.05 1e3c8e9d655401d68a31a5a4c3aca2ca8370cc15

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -38,6 +38,7 @@ uint32_t loader() {
 	/* Load each program segment */
 	//panic("please implement me");
 	ph = (void *)buf + elf->e_phoff;
+	nemu_assert(elf->e_phoff == 52);
 
 	for(; true; ) {
 		/* Scan the program header table, load each segment into memory */
