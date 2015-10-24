aef422fddd0cecb28287d1b157b9b5063ff4ae4d
tracer-ics2015 <tracer@njuics.org>
2015-10-24 20:10:48 +0800

    >  compile kernel 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  20:10:48 up  4:41,  4 users,  load average: 0.00, 0.01, 0.05 d3616a6391d5dabddd1c3f5c8fb1d86bf11c41e3

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -50,7 +50,7 @@ uint32_t loader() {
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			ramdisk_write(buf + ph->p_offset, ph->p_vaddr, ph->p_filesz);
+			ramdisk_write((void *)ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_vaddr, ph->p_filesz);
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
