96bf35946eaf5bae12a4339d6365a8651cfeeafd
tracer-ics2015 <tracer@njuics.org>
2015-11-17 00:52:51 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  00:52:51 up  6:21,  4 users,  load average: 0.00, 0.01, 0.05 5d285e23960e83071c909798fbed2299cffcc478

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -47,7 +47,7 @@ void init_cache() {
 }
 
 static void cache_read_inner(hwaddr_t addr, void *temp) {
-	Log("(cache_read_inner) addr = %x", addr);
+	//Log("(cache_read_inner) addr = %x", addr);
 	Assert(addr < HW_MEM_SIZE, "physical address %x is outside of the physical memory!", addr);
 
 	cache_addr.addr = addr & ~BURST_MASK;
@@ -80,7 +80,7 @@ static void cache_read_inner(hwaddr_t addr, void *temp) {
 }
 
 uint32_t cache_read(hwaddr_t addr, size_t len) {
-	Log("(cache_read) addr = %x", addr);
+	//Log("(cache_read) addr = %x", addr);
 	uint32_t offset = addr & BURST_MASK;
 	uint8_t temp[2 * BURST_LEN];
 
