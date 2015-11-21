e0e21bfd512c27b765e8470c9a4603ea1ef5e501
tracer-ics2015 <tracer@njuics.org>
2015-11-21 16:15:49 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  16:15:49 up  4:32,  4 users,  load average: 0.00, 0.07, 0.09 2ba7e2f7ee82c8651710bf5cbefd39351a356c51

--- nemu/src/memory/L2_cache.c
+++ nemu/src/memory/L2_cache.c
@@ -142,7 +142,7 @@ static void L2_cache_write_inner(hwaddr_t addr, void *temp, void *mask) {
 		}
 
 
-		/* then write the dram *x/
+		/* then write the dram */
 		uint8_t *temp_data;
 		uint32_t temp_idx;
 		size_t temp_len = 0;
@@ -158,7 +158,7 @@ static void L2_cache_write_inner(hwaddr_t addr, void *temp, void *mask) {
 		}
 		Assert(temp_len<=4, "temp_len is flowed");
 		if(temp_len) dram_write(cache_addr.addr + temp_idx, temp_len, *(uint32_t *)temp_data);
-		*/
+		
 
 		/* at last load the slot */
 		for(i = 0; i < ((SLOT_SIZE)/4); ++i) {
@@ -169,7 +169,7 @@ static void L2_cache_write_inner(hwaddr_t addr, void *temp, void *mask) {
 		slot->dirty = 0;
 		slot->tag = cache_addr.tag_idx;
 
-		memcpy_with_mask(slot->data + (cache_addr.addr & SLOT_MASK), temp, BURST_LEN, mask);//?
+		//memcpy_with_mask(slot->data + (cache_addr.addr & SLOT_MASK), temp, BURST_LEN, mask);//?
 
 	}
 }
@@ -205,7 +205,7 @@ void L2_cache_write(hwaddr_t addr, size_t len, uint32_t data) {
 		L2_cache_write_inner(addr + BURST_LEN, temp + BURST_LEN, mask + BURST_LEN);
 	}
 	
-	dram_write(addr, len, data);
+	//dram_write(addr, len, data);
 }
 
 void L2_cache_check(hwaddr_t addr) {
