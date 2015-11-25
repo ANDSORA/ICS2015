c05b2439492aa0d9bcba410408b54924aa45cc0c
tracer-ics2015 <tracer@njuics.org>
2015-11-25 17:53:14 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  17:53:14 up  7:24,  4 users,  load average: 0.00, 0.01, 0.05 91501854c5e1bf9422ac9e49f14710a14dedae2

--- nemu/src/cpu/exec/data-mov/mov-template.h
+++ nemu/src/cpu/exec/data-mov/mov-template.h
@@ -30,8 +30,9 @@ make_helper(concat(mov_moffs2a_, SUFFIX)) {
 
 #if DATA_BYTE == 4
 make_helper(mov_c2r_l) {
-	int len = decode_r_l(eip + 2);
-	printf("(eip==0x%x)0x%x\n",eip+2,instr_fetch(eip+2,1));
+	int len = decode_r_l(eip + 1);
+	printf("(eip==0x%x)0x%x",eip+2,instr_fetch(eip+1,1));
+	printf("len==%u\n",len);
 	REG(op_src->reg) = cpu.cr0.val;
 	print_asm("movl cr0,%%%s", REG_NAME(op_src->reg));
 	return len + 2;
@@ -39,6 +40,8 @@ make_helper(mov_c2r_l) {
 
 make_helper(mov_r2c_l) {
 	int len = decode_r_l(eip + 1);
+	printf("(eip==0x%x)0x%x",eip+2,instr_fetch(eip+1,1));
+	printf("len==%u\n",len);
 	cpu.cr0.val = REG(op_src->reg);
 	print_asm("movl %%%s,cr0", REG_NAME(op_src->reg));
 	return len + 2;
