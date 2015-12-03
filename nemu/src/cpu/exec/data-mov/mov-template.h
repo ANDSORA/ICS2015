78bdff6ea04a32ecf3a1693430518d9d7eedefe3
tracer-ics2015 <tracer@njuics.org>
2015-12-03 10:45:43 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  10:45:43 up  2:40,  3 users,  load average: 0.00, 0.01, 0.05 2bbb14a2fdc536681487bc511d9ee2833f44f003

--- nemu/src/cpu/exec/data-mov/mov-template.h
+++ nemu/src/cpu/exec/data-mov/mov-template.h
@@ -45,7 +45,12 @@ make_helper(mov_c2rm_l) {
 	int len = decode_rm_l(eip + 1);
 	//printf("(eip==0x%x)0x%x ",eip+2,instr_fetch(eip+1,1));
 	//printf("len==%u\n",len);
-	REG(op_src->reg) = cpu.cr0.val;
+	switch(op_src2->reg){
+		case 0: REG(op_src->reg) = cpu.cr0.val; break;
+		case 3: REG(op_src->reg) = cpu.cr3.val; break;
+		default: panic("No such control register!"); break;
+	}
+	//REG(op_src->reg) = cpu.cr0.val;
 	print_asm("movl cr0,%%%s", REG_NAME(op_src->reg));
 	return len + 1;
 }
@@ -54,7 +59,12 @@ make_helper(mov_rm2c_l) {
 	int len = decode_rm_l(eip + 1);
 	//printf("(eip==0x%x)0x%x ",eip+2,instr_fetch(eip+1,1));
 	//printf("len==%u\n",len);
-	cpu.cr0.val = REG(op_src->reg);
+	switch(op_src2->reg){
+		case 0: cpu.cr0.val = REG(op_src->reg); break;
+		case 3: cpu.cr3.val = REG(op_src->reg); break;
+		default: panic("No such control register!"); break;
+	}
+	//cpu.cr0.val = REG(op_src->reg);
 	print_asm("movl %%%s,cr0", REG_NAME(op_src->reg));
 	return len + 1;
 }
