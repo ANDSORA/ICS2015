b905fe5461bd76b0ec93599ecbce40578dda5168
tracer-ics2015 <tracer@njuics.org>
2015-12-15 00:03:15 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  00:03:15 up  2:40,  4 users,  load average: 0.00, 0.01, 0.05 b18f6d3e8dc2d988b3e88793edd312dd8aa70ed

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -228,9 +228,9 @@ helper_fun _2byte_opcode_table [256] = {
 };
 
 make_helper(exec) {
-	printf("(exec) ");
+	//printf("(exec) ");
 	ops_decoded.opcode = instr_fetch(eip, 1);
-	printf("%x\n",ops_decoded.opcode);
+	printf("(exec) %x\n",ops_decoded.opcode);
 	return opcode_table[ ops_decoded.opcode ](eip);
 }
 
