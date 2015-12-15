74d4c52e056c492f560606f74eacf8afc0f460ab
tracer-ics2015 <tracer@njuics.org>
2015-12-16 00:27:54 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  00:27:54 up  2:25,  2 users,  load average: 0.00, 0.01, 0.05 8ff2463b7028632c9a72c8493b47cc5c62cdb1db

--- nemu/src/cpu/exec/misc/misc.c
+++ nemu/src/cpu/exec/misc/misc.c
@@ -41,7 +41,7 @@ make_helper(lgdt) {
 	//swaddr_t addr = instr_fetch(eip + 2, 4);
 	cpu.gdtr.limit = lnaddr_read(op_src->val, 2);//buggy?
 	cpu.gdtr.base = lnaddr_read(op_src->val + 2, 4);//buggy?
-	printf("len=%x addr=%x limit=%x base=%x\n",len,op_src->val,cpu.gdtr.limit,cpu.gdtr.base);
+	printf("len=%x val=%x addr=%x limit=%x base=%x\n",len,op_src->val,op_src->addr,cpu.gdtr.limit,cpu.gdtr.base);
 	print_asm("lgdt 0x%x:0x%x", cpu.gdtr.limit, cpu.gdtr.base);
 	return len + 1;
 }
