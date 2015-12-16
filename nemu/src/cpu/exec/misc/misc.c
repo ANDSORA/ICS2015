b0cd9404aac377bd9d666c51975c4fdfee1865e2
tracer-ics2015 <tracer@njuics.org>
2015-12-16 18:32:14 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  18:32:14 up  4:35,  3 users,  load average: 0.02, 0.02, 0.05 811779b4fd633a178609117e129972e2a41cfec9

--- nemu/src/cpu/exec/misc/misc.c
+++ nemu/src/cpu/exec/misc/misc.c
@@ -71,3 +71,17 @@ make_helper(lidt) {
 	print_asm("lidt\t0x%x", op_src->addr);
 	return len + 1;
 }
+
+make_helper(pusha) {
+	uint32_t temp = cpu.esp;
+	cpu.esp -= 4; swaddr_write(cpu.esp, 4, cpu.eax, R_SS);
+	cpu.esp -= 4; swaddr_write(cpu.esp, 4, cpu.ecx, R_SS);
+	cpu.esp -= 4; swaddr_write(cpu.esp, 4, cpu.edx, R_SS);
+	cpu.esp -= 4; swaddr_write(cpu.esp, 4, cpu.ebx, R_SS);
+	cpu.esp -= 4; swaddr_write(cpu.esp, 4, temp, R_SS);
+	cpu.esp -= 4; swaddr_write(cpu.esp, 4, cpu.ebp, R_SS);
+	cpu.esp -= 4; swaddr_write(cpu.esp, 4, cpu.esi, R_SS);
+	cpu.esp -= 4; swaddr_write(cpu.esp, 4, cpu.edi, R_SS);
+	print_asm("pusha");
+	return 1;
+}
