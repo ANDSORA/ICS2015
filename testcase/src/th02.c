4ba0a2cdc25bcb67e7579e9280882a1d29ddfd12
tracer-ics2015 <tracer@njuics.org>
2015-12-17 15:53:15 +0800

    >  run 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  15:53:15 up  1:40,  2 users,  load average: 0.00, 0.01, 0.05 5190a150c7067c25a243b772756cb7cfd98c06eb

--- testcase/src/th02.c
+++ testcase/src/th02.c
@@ -62,7 +62,7 @@ int isatty(int fd) {
 int main() {
 	//printf("%s", "Hello world!\n");
 	//printf("%d + %d = %d\n", 1, 1, 2);
-	printf("%d\n", 1);
+	printf("%c\n", 'a');
 
 	HIT_GOOD_TRAP;
 	return 0;
