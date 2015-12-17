b00f398c2abc6243fd5880aaa30b8fc3f5a4a682
tracer-ics2015 <tracer@njuics.org>
2015-12-17 22:59:15 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  22:59:15 up  8:46,  3 users,  load average: 0.09, 0.04, 0.05 d17cf5defba073a12694603bfac88283b01f6ea9

--- nemu/src/cpu/exec/data-mov/cmovcc-template.h
+++ nemu/src/cpu/exec/data-mov/cmovcc-template.h
@@ -5,8 +5,8 @@ static void concat3(instr,_execute_,SUFFIX)() {
 	print_asm( str(instr) " %s %s",op_src->str,op_dest->str);
 }
 
-make_helper(concat3(instr,_r2rm_,SUFFIX)){
-	return idex(eip,concat(decode_r2rm_,SUFFIX),concat3(instr,_execute_,SUFFIX));
+make_helper(concat3(instr,_rm2r_,SUFFIX)){
+	return idex(eip,concat(decode_rm2r_,SUFFIX),concat3(instr,_execute_,SUFFIX));
 }
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/data-mov/cmovcc.h
+++ nemu/src/cpu/exec/data-mov/cmovcc.h
@@ -1,21 +1,21 @@
 #ifndef __CMOVCC_H__
 #define __CMOVCC_H__
 
-make_helper(cmovo_r2rm_v);
-make_helper(cmovno_r2rm_v);
-make_helper(cmovb_r2rm_v);
-make_helper(cmovae_r2rm_v);
-make_helper(cmove_r2rm_v);
-make_helper(cmovne_r2rm_v);
-make_helper(cmovbe_r2rm_v);
-make_helper(cmova_r2rm_v);
-make_helper(cmovs_r2rm_v);
-make_helper(cmovns_r2rm_v);
-make_helper(cmovp_r2rm_v);
-make_helper(cmovnp_r2rm_v);
-make_helper(cmovl_r2rm_v);
-make_helper(cmovge_r2rm_v);
-make_helper(cmovle_r2rm_v);
-make_helper(cmovg_r2rm_v);
+make_helper(cmovo_rm2r_v);
+make_helper(cmovno_rm2r_v);
+make_helper(cmovb_rm2r_v);
+make_helper(cmovae_rm2r_v);
+make_helper(cmove_rm2r_v);
+make_helper(cmovne_rm2r_v);
+make_helper(cmovbe_rm2r_v);
+make_helper(cmova_rm2r_v);
+make_helper(cmovs_rm2r_v);
+make_helper(cmovns_rm2r_v);
+make_helper(cmovp_rm2r_v);
+make_helper(cmovnp_rm2r_v);
+make_helper(cmovl_rm2r_v);
+make_helper(cmovge_rm2r_v);
+make_helper(cmovle_rm2r_v);
+make_helper(cmovg_rm2r_v);
 
 #endif
