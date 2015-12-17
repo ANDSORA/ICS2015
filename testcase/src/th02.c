f3225d35d600836630ca3c0281a3bdda3d78d1cf
tracer-ics2015 <tracer@njuics.org>
2015-12-17 17:07:12 +0800

    >  run 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  17:07:12 up  2:54,  2 users,  load average: 0.11, 0.07, 0.06 46aa31e4d0720e14f4f53500f391fbdc99b4d16d

--- testcase/src/th02.c
+++ testcase/src/th02.c
@@ -62,6 +62,7 @@ int isatty(int fd) {
 int main() {
 	//printf("%s", "Hello world!\n");
 	//printf("%d + %d = %d\n", 1, 1, 2);
+	printf("WTF!\n");
 	printf("%d\n", 34);
 	//printf("WTF!\n");
 	//printf("still_WTF!\n");
