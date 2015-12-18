895c358eb8ab9f5e07981024cd35afd820096e0e
tracer-ics2015 <tracer@njuics.org>
2015-12-18 23:49:12 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  23:49:12 up  7:56,  2 users,  load average: 0.03, 0.04, 0.05 2ce95b2f8d3f858d6fedf381a897caa1e5e6c515

--- nemu/src/cpu/exec/misc/misc.c
+++ nemu/src/cpu/exec/misc/misc.c
@@ -112,7 +112,7 @@ make_helper(hlt) {
 
 make_helper(sti) {
 	printf("(sti)1. EIP==0x%x\n", cpu.eip);
-	cpu.TF = 1;
+	cpu.DF = 1;
 	printf("(sti)2. EIP==0x%x\n", cpu.eip);
 	print_asm("sti");
 	printf("(sti)3. EIP==0x%x\n", cpu.eip);
