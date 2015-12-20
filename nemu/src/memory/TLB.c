2cb62e82a5a1a3f695e0788be338836f4f4fff7b
tracer-ics2015 <tracer@njuics.org>
2015-12-21 01:17:36 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  01:17:36 up  8:53,  2 users,  load average: 0.01, 0.05, 0.05 81d457838db32bd94387c563bb61f5a111aff4bc

--- nemu/src/memory/TLB.c
+++ nemu/src/memory/TLB.c
@@ -50,11 +50,11 @@ hwaddr_t tlb_read(lnaddr_t addr){
 
 		hwaddr_t dir_addr = (cpu.cr3.val & 0xfffff000) + dir_idx*4;
 		uint32_t dir = hwaddr_read(dir_addr, 4);
-		Assert(dir&0x1,"FUCK, MISS IN PDE!\tDIR == 0x%x", dir);
+		Assert(dir&0x1,"FUCK, MISS IN PDE!\tDIR_addr == 0x%x", dir_addr);
 
 		hwaddr_t page_addr = (dir & 0xfffff000) + page_idx*4;
 		uint32_t page = hwaddr_read(page_addr, 4);
-		Assert(page&0x1,"FUCK, MISS IN PTE!\tTABLE == 0x%x", page);
+		Assert(page&0x1,"FUCK, MISS IN PTE!\tTABLE_addr == 0x%x", page_addr);
 
 		//tlb[target].ptable.val = page;
 		tlb[target].page = page;
