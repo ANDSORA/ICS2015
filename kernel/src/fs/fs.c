46c08c1fe6e4c510c948118e080f9b041e1d8d7e
tracer-ics2015 <tracer@njuics.org>
2015-12-21 01:23:02 +0800

    >  compile kernel 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  01:23:02 up  8:58,  2 users,  load average: 0.00, 0.02, 0.05 e452bee6c0581b7a35e28b8330eb086f4e859faa

--- kernel/src/fs/fs.c
+++ kernel/src/fs/fs.c
@@ -87,9 +87,10 @@ int fs_lseek(int fd, int offset, int whence) {
 	switch(whence) {
 		case SEEK_SET: new_offset = offset; break;
 		case SEEK_CUR: new_offset = files[fd].offset + offset; break;
-		case SEEK_END: Log("Are you crazy?"); new_offset = file_table[fd - 3].size + offset; break;
+		case SEEK_END: new_offset = file_table[fd - 3].size + offset; break;
 		default: assert(0); break;
 	}
+	assert(new_offset >=0 && new_offset < file_table[fd - 3].size);
 	files[fd].offset = new_offset;
 	return new_offset;
 }
