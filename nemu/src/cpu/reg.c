bd0504df6a3f39186058fd65979b494563646e45
tracer-ics2015 <tracer@njuics.org>
2015-12-29 11:16:38 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  11:16:38 up 8 min,  2 users,  load average: 0.09, 0.07, 0.05 d9717f51eea427e143a6d8adc2f9fd61fcf16232

--- nemu/src/cpu/reg.c
+++ nemu/src/cpu/reg.c
@@ -78,7 +78,7 @@ void setEFLAGS_ALU(uint32_t x,uint32_t y,bool cin,uint32_t data_len) {
 void setEFLAGS_LOGIC(uint32_t x,uint32_t data_len){
 	cpu.CF=0;
 	setPF(x);
-	cpu.ZF= x==0;
+	cpu.ZF= (x==0);
 	cpu.SF= ( x>>((data_len<<3)-1) ) & 1;
 	cpu.OF=0;
 }
