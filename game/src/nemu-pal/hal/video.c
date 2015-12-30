ac79cd85639ff6673719bd64fe181d4b414c166f
tracer-ics2015 <tracer@njuics.org>
2015-12-30 17:49:12 +0800

    >  compile game 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  17:49:12 up  3:04,  3 users,  load average: 0.05, 0.07, 0.06 f3702f39a12b8b834590e001ee335b30f2a5871d

--- game/src/nemu-pal/hal/video.c
+++ game/src/nemu-pal/hal/video.c
@@ -82,6 +82,7 @@ void SDL_UpdateRect(SDL_Surface *screen, int x, int y, int w, int h) {
 
 void SDL_SetPalette(SDL_Surface *s, int flags, SDL_Color *colors, 
 		int firstcolor, int ncolors) {
+	Log("SDL_SetPalette");
 	assert(s);
 	assert(s->format);
 	assert(s->format->palette);
