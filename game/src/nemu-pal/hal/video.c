6059e2fb21445a0a8e4730a279bef0e582c1be6a
tracer-ics2015 <tracer@njuics.org>
2015-12-30 17:41:34 +0800

    >  compile game 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  17:41:34 up  2:56,  2 users,  load average: 0.00, 0.02, 0.05 ccf6fb924667367131ce24711e76c0c71c1bee0

--- game/src/nemu-pal/hal/video.c
+++ game/src/nemu-pal/hal/video.c
@@ -55,6 +55,7 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 }
 
 void SDL_UpdateRect(SDL_Surface *screen, int x, int y, int w, int h) {
+	printf("SDL_Surface");
 	assert(screen);
 	assert(screen->pitch == 320);
 	if(screen->flags & SDL_HWSURFACE) {
