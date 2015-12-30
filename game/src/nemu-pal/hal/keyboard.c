286c458b1f966f44e94d4f9f3a0203be86683e9d
tracer-ics2015 <tracer@njuics.org>
2015-12-30 20:27:07 +0800

    >  compile NEMU 141220121 andsora Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  20:27:07 up  5:42,  3 users,  load average: 0.29, 0.08, 0.07 13dc320b367fdedc68f3c95055bd0a9dab4382c

--- game/src/nemu-pal/hal/keyboard.c
+++ game/src/nemu-pal/hal/keyboard.c
@@ -15,6 +15,8 @@ static const int keycode_array[] = {
 static int key_state[NR_KEYS];
 
 static volatile int recent_key_index = -1;
+static volatile int recent_keycode = -1;
+//static volatile int pre_recent_keycode = -1;
 
 void
 keyboard_event(void) {
@@ -22,12 +24,19 @@ keyboard_event(void) {
 	//assert(0);
 	int key_code = in_byte(0x60);
 	int i;
+	recent_keycode = key_code;
+	if(key_code == 0xf0) return;
 	for (i = 0; i < NR_KEYS; i ++){
 		if(keycode_array[i] == key_code) {
-			recent_key_index = i;
-			key_state[i] = KEY_STATE_PRESS;
+			if(recent_key_index == 0xf0) key_state[i] = KEY_STATE_RELEASE;
+			else if(key_state[i] == KEY_STATE_RELEASE || key_state[i] == KEY_STATE_EMPTY) key_state[i] = KEY_STATE_PRESS;
+			else key_state[i] = KEY_STATE_WAIT_RELEASE;
+			break;
 		}
 	}
+	assert(i < NR_KEYS);
+	recent_keycode = key_code;
+	recent_key_index = i;
 }
 
 static inline int
@@ -45,14 +54,14 @@ query_key(int index) {
 static inline void
 release_key(int index) {
 	assert(index >= 0 && index < NR_KEYS);
-	recent_key_index = index;
+	//recent_key_index = index;
 	key_state[index] = KEY_STATE_WAIT_RELEASE;
 }
 
 static inline void
 clear_key(int index) {
 	assert(index >= 0 && index < NR_KEYS);
-	recent_key_index = index;
+	//recent_key_index = index;
 	key_state[index] = KEY_STATE_EMPTY;
 }
 
@@ -72,15 +81,16 @@ process_keys(void (*key_press_callback)(int), void (*key_release_callback)(int))
 	else {
 		int recent_key_state = query_key(recent_key_index);
 		int recent_keycode = get_keycode(recent_key_index);
-		if(recent_key_state == KEY_STATE_PRESS) {
+		if(recent_key_state == KEY_STATE_PRESS || recent_key_state == KEY_STATE_WAIT_RELEASE) {
 			key_press_callback(recent_keycode);
 			sti(); return true;
 		}
-		else if(recent_key_state == KEY_STATE_WAIT_RELEASE) {
+		else if(recent_key_state == KEY_STATE_RELEASE) {
 			key_release_callback(recent_keycode);
 			sti(); return true;
 		}
 		else {
+			assert(0);
 			sti(); return false;
 		}
 	}
